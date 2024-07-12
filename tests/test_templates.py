844a526ad7e925343395cd75184b4a0d97a6844c
ajorig <152420132+ajorig@users.noreply.github.com>
2024-07-12 22:57:15 +0200

    fix unit test: test_not_find_best_template by using the image from the right step

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -16,8 +16,8 @@ def test_find_best_template(tmp_path, crop_img_files):
 
 def test_not_find_best_template(tmp_path, base_img_files):
     # TODO improve test using a image that could be a consent form
-    folder = base_img_files.files.get(StepEnum.ROTATED).path.parent
-    root_filename = base_img_files.files.get(StepEnum.ROTATED).path.stem.split(i.CONVERT_PAGE_SEPARATOR)
+    folder = base_img_files.files.get(StepEnum.BASE).path.parent
+    root_filename = base_img_files.files.get(StepEnum.BASE).path.stem.split(i.CONVERT_PAGE_SEPARATOR)
     img_filepath = path.join(folder, f"{root_filename}{i.CONVERT_PAGE_SEPARATOR}0{i.CONVERT_PNG_EXT}")
     svg_template_filepath = t.find_best_template(FilePath(img_filepath),
                                                  CONFIG_PAGES,
